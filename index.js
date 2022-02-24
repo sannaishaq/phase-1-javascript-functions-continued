951c263ec9dca9e772afc7e2707395cd9d2ff4b4
Sanna Ishaq <sakinah.ishaq@gmail.com>
2022-02-23 20:38:57 -0500

    commit tree 03f6b0165f54930aae5784cbfe33ec657da63621

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// code your solution here
+// function razzle(lawyer = "Billy", target = "'em") {
+//     console.log(`${lawyer} razzle-dazzles ${target}!`);
+//   }
+//   razzle(); //=> Billy razzle-dazzles 'em!
+//   razzle("Methuselah", "T'challah"); //=> Methuselah razzle-dazzles T'challah!
+ // if(activity != 'roller-skate'){
+    //     return activity = 'bathe my dog'
+    // }
+
+function saturdayFun(activity = 'roller-skate'){
+    return (`This Saturday, I want to ${activity}!`);
+}
+   
+    
+function mondayWork(activity = 'go to the office'){
+    return (`This Monday, I will ${activity}.`);
+}
+
+function wrapAdjective(wrap){
+    let innerFunction = function (x= "a hard worker"){
+        return (`You are ${wrap}${x}${wrap}!`)
+    }
+    return innerFunction
+}
+
+
+
+
+
+
+// let result = wrapAdjective('*')
+    // let emphatic = result("a hard worker")
+    // if(result = true){
+    //     return result+emphatic
+    // }else{
+    //    let result = wrapAdjective("||")
+    //    let emphatic = result("a dedicated programmer")
+    //    return result+emphatic
+
+    // }
